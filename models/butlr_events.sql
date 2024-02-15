1c91e920a2c4d594b28e7dc6a6fd998369881057
shrawan-sngy <shrawan.bagariya@synergy-ai.us>
2024-02-15 11:12:54 +0530

    Added dbt model to process butlr events

--- models/butlr_events.sql
+++ models/butlr_events.sql
@@ -1,25 +1,45 @@
-{{ config(
-    materialized='incremental',
-    unique_key=['building_id', 'building_code', 'mac_address'],
-    merge_update_columns=['started_from', 'end_date', 'created_date'],
-    schema='processed_events',
-    alias='butlr_transformation'
-) }}
+{{
+    config(
+        materialized='incremental',
+        partition_by={
+            "field": "created_date",
+            "data_type": "timestamp",
+            "granularity": "day"
+        },
+        cluster_by = ['created_date'],
+        unique_key = ['timestamp', 'mac_address'],
+        merge_update_columns = ['value', 'created_date']
+    )
+}}
 
 WITH source_data AS (
     SELECT
-        CAST(building_id AS int64) AS building_id,
-        CAST(building_code AS string) AS building_code,
-        CAST(mac_address AS string) AS mac_id,
-        CAST(start AS timestamp) AS started_from,
-        CAST(stop AS timestamp) AS end_date,
-        CAST(time AS timestamp) AS created_date,
-        'butlr' AS data_source
-    FROM `transformed_events.BUTLR_Connector`
+        JSON_EXTRACT_SCALAR(object, '$.building_id') AS building_id,
+        JSON_EXTRACT_SCALAR(object, '$.field') AS field,
+        JSON_EXTRACT_SCALAR(object, '$.time') AS timestamp,
+        JSON_EXTRACT_SCALAR(object, '$.start') AS start,
+        JSON_EXTRACT_SCALAR(object, '$.stop') AS stop,
+        JSON_EXTRACT_SCALAR(object, '$.value') AS value,
+        JSON_EXTRACT_SCALAR(object, '$.timezone_offset') AS timezone_offset,
+        JSON_EXTRACT_SCALAR(object, '$.building_name') AS building_name,
+        JSON_EXTRACT_SCALAR(object, '$.hive_id') AS hive_id,
+        JSON_EXTRACT_SCALAR(object, '$.hive_serial') AS hive_serial,
+        JSON_EXTRACT_SCALAR(object, '$.sensor_id') AS sensor_id,
+        JSON_EXTRACT_SCALAR(object, '$.space_id') AS space_id,
+        JSON_EXTRACT_SCALAR(object, '$.space_name') AS space_name,
+        JSON_EXTRACT_SCALAR(object, '$.room_id') AS room_id,
+        JSON_EXTRACT_SCALAR(object, '$.room_name') AS room_name,
+        JSON_EXTRACT_SCALAR(object, '$.zone_id') AS zone_id,
+        JSON_EXTRACT_SCALAR(object, '$.zone_name') AS zone_name,
+        JSON_EXTRACT_SCALAR(object, '$.mac_address') AS mac_address,
+        _airbyte_extracted_at AS created_date,
+        "Butlr" as data_source
+    FROM `podium-datalake-dev-38a8.transformed_events.BUTLR_Connector`,
+        UNNEST(JSON_EXTRACT_ARRAY(data, '$')) AS object;
 
     {% if is_incremental() %}
-    WHERE time > (SELECT MAX(created_date) FROM {{ this }})
+        WHERE _airbyte_extracted_at > (SELECT MAX(created_date) FROM {{ this }})
     {% endif %}
 )
 
-SELECT * FROM source_data;
+SELECT * FROM source_data
