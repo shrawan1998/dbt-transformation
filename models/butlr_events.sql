3760fe3644f2c11bf9c02d91f131ca737700bbd8
ukais <ukaissyed@gmail.com>
2024-02-15 10:38:22 +0530

    butlr

--- models/butlr_events.sql
+++ models/butlr_events.sql
@@ -1,25 +1,25 @@
-{{config
-(materialized='incremental',
-unique_key =['building_id','building_code','mac_address'],
-merge_update_columns=['started_from','end_date','created_date'],
-schema='processed_events',
-alias='butlr_transformation')}}
+{{ config(
+    materialized='incremental',
+    unique_key=['building_id', 'building_code', 'mac_address'],
+    merge_update_columns=['started_from', 'end_date', 'created_date'],
+    schema='processed_events',
+    alias='butlr_transformation'
+) }}
 
-select source_data AS (
-    CAST(building_id AS int64) AS building_id,
-    CAST(building_code AS string) AS building_code,
-    CAST(mac_address AS string) AS mac_id,
-    CAST(start AS timestamp) AS started_from,
-    CAST(stop AS timestamp) AS end_date,
-    CAST(time AS timestamp) AS created_date,
-    'butlr' AS data_source
+WITH source_data AS (
+    SELECT
+        CAST(building_id AS int64) AS building_id,
+        CAST(building_code AS string) AS building_code,
+        CAST(mac_address AS string) AS mac_id,
+        CAST(start AS timestamp) AS started_from,
+        CAST(stop AS timestamp) AS end_date,
+        CAST(time AS timestamp) AS created_date,
+        'butlr' AS data_source
     FROM `transformed_events.BUTLR_Connector`
 
-
-    {%if is_incremental()%}
-    WHERE time >(SELECT MAX(created_date)) FROM {{this}}
-   
+    {% if is_incremental() %}
+    WHERE time > (SELECT MAX(created_date) FROM {{ this }})
+    {% endif %}
 )
-select * from source_data
 
- {%endif%}
+SELECT * FROM source_data;
